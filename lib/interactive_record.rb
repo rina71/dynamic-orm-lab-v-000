31029194a6c72da1cb41a1a97eae372fe2dff926
Yurina Shin <yurinas@gmail.com>
2018-07-15 20:23:21 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,71 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash
+    sql = "PRAGMA table_info('#{self.table_name}')"
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+    table_info.each{|hash|
+      column_names << hash["name"]}
+    column_names
+  end
+
+  def table_name_for_insert
+    self.class.table_name
+  end
+
+  def col_names_for_insert
+    self.class.column_names.delete_if{|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each{|col_name| values << "'#{send(col_name)}'" unless send(col_name) == nil}
+    values.join(", ")
+  end
+
+  def save
+    sql = <<-SQL
+    INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+    VALUES (#{values_for_insert})
+    SQL
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT *
+    FROM #{self.table_name}
+    WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name)
+  end
+
+  def self.find_by(hash)
+    # binding.pry
+    col_name = hash.keys[0].to_s
+    value = hash.values[0]
+    # formatted_value = value.class == Fixnum ? value : "'#{value}'"
+    if value.is_a? Numeric
+      formatted_value = value
+    else
+      formatted_value = "'#{value}'"
+    end
+
+    sql = <<-SQL
+    SELECT *
+    FROM #{self.table_name}
+    WHERE #{col_name} = #{formatted_value}
+    SQL
+    # binding.pry
+    DB[:conn].execute(sql)
+  end
 end
