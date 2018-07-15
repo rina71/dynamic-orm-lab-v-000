52968ae94af66be71c619b6d26363679965a8916
Yurina Shin <yurinas@gmail.com>
2018-07-15 18:51:00 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -52,12 +52,12 @@ class InteractiveRecord
 
   def self.find_by(hash)
     binding.pry
-    col_name = hash.keys.to_s 
+    col_name = hash.keys.to_s
     value = hash.values
     sql = <<-SQL
     SELECT *
     FROM #{self.table_name}
-    WHERE 
+    WHERE
     SQL
 
     DB[:conn].execute(sql)
